426784b1b80a0b8526e3dfdf67b23a28d23a028d
waqas <waqas@opensource.com.pk>
2017-05-08 23:55:18 +0500

    a

--- serve.py
+++ serve.py
@@ -1,5 +1,10 @@
 from gevent.wsgi import WSGIServer
-from app.init_app import app
+from app.init_app import app, init_app, manager
 
-http_server = WSGIServer(('0.0.0.0', 8080), app)
+http_server = WSGIServer(('127.0.0.1', 8080), app)
+init_app(app)
 http_server.serve_forever()
+
+# if __name__ == "__main__":
+#     init_app(app)
+#     app.run()
